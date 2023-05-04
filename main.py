13cb8751674cb59b63707d3cabbe39dd4be786bf
Solyarich <ivanikita111997@yandex.ru>
2023-05-04 21:47:15 +0300

    Add requests

--- main.py
+++ main.py
@@ -17,6 +17,9 @@ API = os.getenv('API')
 bot = telebot.TeleBot(API)
 db = Database('database.db')
 
+conn_req = sqlite3.connect('requests.db', check_same_thread=False)
+c_req = conn_req.cursor()
+
 udb = pickledb.load('users.db', False)
 
 logging.basicConfig( 
@@ -24,6 +27,9 @@ logging.basicConfig(
     level=logging.INFO, 
 ) 
 logger = logging.getLogger(__name__) 
+
+name = ''
+surname = ''
 
 def getid(id):
     exist = udb.get(id)
@@ -48,15 +54,71 @@ def start(message):
             else:
                 udb.set(str(message.from_user.id), "user")
                 udb.dump()
-                bot.send_message(message.chat.id, "Добро пожаловать! Что бы вы хотели узнать?")
         if udb.get(str(message.from_user.id)) == "admin":
             bot.send_message(message.from_user.id, 'Добро пожаловать, Администратор!')
         elif udb.get(str(message.from_user.id)) == "purch":
             bot.send_message(message.from_user.id, 'Добро пожаловать, хотели бы вы что-то заказать или узнать?')
         else:
             bot.send_message(message.from_user.id, 'Добро пожаловать! Что вас интересует?')
+    elif message.text == 'Запрос':
+        if udb.get(str(message.from_user.id)) == "user":
+            global name
+            global surname
+            name = ''
+            surname = ''
+            bot.send_message(message.from_user.id, 'Пожалуйста, напишите своё имя.')
+            bot.register_next_step_handler(message, request)
+        elif udb.get(str(message.from_user.id)) == False:
+            refusal(message.from_user.id)
+        else:
+            bot.send_message(message.from_user.id, 'У вас уже есть эти права.')
+    elif message.text == 'Принять запрос':
+        if udb.get(str(message.from_user.id)) == "user":
+            bot.send_message(message.from_user.id, 'У вас недостаточно прав.')
+        else:
+            bot.send_message(message.from_user.id, 'Текущие запросы:')
+            c_req.execute("""SELECT * from Requests""")
+            records = c_req.fetchall()
+            for row in records:
+                bot.send_message(message.from_user.id, "1")
+                bot.send_message(message.from_user.id, row[1] + " " + row[2] + ", ID - " + row[3])
+            bot.send_message(message.from_user.id, 'Если хотите принять запрос, напишите id пользователя или напишите "Отмена" в ином случае')
+            bot.register_next_step_handler(message, accept_requests)
     else:
-        bot.send_message(message.from_user.id, 'Пожалуйста, напишите /start для регистрации')
-        
+        refusal(message.from_user.id)
+
+def refusal(id):
+    bot.send_message(id, 'Пожалуйста, напишите /start для регистрации.')
+
+def request(message):
+    global name 
+    name = message.text
+    bot.send_message(message.from_user.id, name)
+    bot.send_message(message.from_user.id, 'Пожалуйста, напишите свою фамилию.')
+    bot.register_next_step_handler(message, request_2)
+
+def request_2(message):
+    global surname
+    global name
+    surname = message.text
+    c_req.execute('INSERT INTO Requests (Name, Surname, Teleg_ID) VALUES (?,?,?)', (name, surname, str(message.from_user.id)))
+    conn_req.commit()
+    name = ''
+    surname = ''
+    bot.send_message(message.from_user.id, 'Запрос создан.')
+
+def accept_requests(message):
+    if message.text != "Отмена":
+        ID = str(message.text)
+        del_str = """Delete from Requests where Teleg_ID = ?"""
+        c_req.execute(del_str, (ID, ))
+        conn_req.commit()
+        udb.set(ID, "purch")
+        udb.dump()
+        bot.send_message(message.from_user.id, 'Запрос принят.')
+    else:
+        bot.send_message(message.from_user.id, 'Закрытие запросов.')
+
+
 
 bot.polling()
