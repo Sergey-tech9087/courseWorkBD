96134ee28f7edd825fb477f54b308ab068a5c4ba
Solyarich <ivanikita111997@yandex.ru>
2023-05-04 20:10:38 +0300

    Add user_db

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import requests
 import ast
 from db import Database
 import sqlite3
+import pickledb
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -16,18 +17,33 @@ API = os.getenv('API')
 bot = telebot.TeleBot(API)
 db = Database('database.db')
 
+udb = pickledb.load('users.db', False)
+
 logging.basicConfig( 
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", 
     level=logging.INFO, 
 ) 
 logger = logging.getLogger(__name__) 
 
+def getid(id):
+    exist = udb.get(id)
+    if exist == False:
+        return False
+    else:
+        return True
 
 
 # Команда /start
 @bot.message_handler(commands=['start']) 
-def start_quiz(message):
-    bot.send_message(message.chat.id, "result")
-
+def start(message):
+    id_f = getid(str(message.from_user.id))
+    if not id_f:
+        if str(message.from_user.id) == "642629943":
+            udb.set(str(message.from_user.id), "admin")
+            udb.dump()
+        else:
+            udb.set(str(message.from_user.id), "user")
+            udb.dump()
+    bot.send_message(message.chat.id, "Добро пожаловать! Что бы вы хотели узнать?")
 
 bot.polling()
